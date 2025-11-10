775b91a33c08886c109e1af4fe2fe3596cd8ee9e
DeveloPauMoGar <paumogar.dev@gmail.com>
2025-11-10 10:52:49 +0100

    Create ejercicio2.js

---
+++ taller1/ejercicio2.js
@@ -0,0 +1,11 @@
+let nota = 8;
+
+if (nota >= 9 && nota <= 10) {
+  console.log("Excelente");
+} else if (nota >= 7 && nota < 9) {
+  console.log("Muy bueno");
+} else if (nota >= 5 && nota < 7) {
+  console.log("Bueno");
+} else {
+  console.log("Suspenso");
+}
