50d424da0d21909afa037a49a3538e96845bff18
DeveloPauMoGar <paumogar.dev@gmail.com>
2025-11-10 10:44:44 +0100

    Create ejercicio1.js

---
+++ taller1/ejercicio1.js
@@ -0,0 +1,8 @@
+const nombre_empresa = "TechCorp";
+let empleado = "Paula";
+let salario = 1235;
+
+empleado= 2;
+salario = "Mil doscientos treinta y cinco";
+console.log(typeof empleado);
+console.log(typeof salario);
