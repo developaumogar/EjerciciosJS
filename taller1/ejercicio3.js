957624768e0b071c051fd913d16091e0983151b0
DeveloPauMoGar <paumogar.dev@gmail.com>
2025-11-10 11:20:52 +0100

    a

---
+++ taller1/ejercicio3.js
@@ -0,0 +1,19 @@
+for (let i = 2; i<= 20 ; i+=2){
+    console.log(i);
+}
+
+let j = 1;
+while (j <= 15) {
+    if (j % 2 != 0) {
+        
+        console.log(j);
+    }
+    j++;
+}
+
+let k = 5;
+do {
+    
+    console.log("El usuario escribió: " + k);
+    k--;
+} while (k != 0);
